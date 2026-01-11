28a00f6531bbba68c4a2b0c23eed65cced27f9e4
ewgsta <159681870+ewgsta@users.noreply.github.com>
2026-01-11 15:43:09 +0300

    Update weeb-cli to latest version

--- Formula/weeb-cli.rb
+++ Formula/weeb-cli.rb
@@ -3,8 +3,8 @@ class WeebCli < Formula
 
   desc "Tarayıcı yok, reklam yok. Sadece sen ve anime."
   homepage "https://github.com/ewgsta/weeb-cli"
-  url "https://files.pythonhosted.org/packages/source/w/weeb-cli/weeb-cli-0.1.0.tar.gz"
-  sha256 "98e8315ff8a24b41a049cf17015099a290fe6e800f7299051080b671de682a84"
+  url "https://files.pythonhosted.org/packages/source/w/weeb-cli/weeb-cli-0.1.1.tar.gz"
+  sha256 "60e75f6bf6f1519eeab421aff66692ca02b354e2aa5ae91f919e546635cf0d6e"
   license "CC-BY-NC-ND-4.0"
 
   depends_on "python@3.11"
